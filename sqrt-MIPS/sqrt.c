9d5aa66518cc3e7b54121ae3d3951c77b318bff2
Mateus Oliveira <o.mateus.p@gmail.com>
2019-11-05 21:18:41 -0300

    Update sqrt.c

--- sqrt-MIPS/sqrt.c
+++ sqrt-MIPS/sqrt.c
@@ -6,12 +6,8 @@ double encontra_inteiros(double x)
   for (i = 2; i2 < x; i++)
   {
     i2 = i * i;
-    // printf("i2 = %d\n", i2);
   }
-  // i2 = i - 1;
   i -= 2;
-  // printf("a = %f\nb = %f\n", i, i2);
-  // calc_raiz(i, i2, x);
   return i;
 }
 
@@ -20,8 +16,6 @@ void calc_raiz(double a1, double x, int i, int e)
   double a2, b1;
   b1 = x / a1;
   a2 = (a1 + b1) / 2.0;
-  //printf("b%d = %.13f\n", i + 1, a2);
-  // printf("b1= %lf x= %lf\n", b1, x);
   if (i > 100)
   {
     printf("Nao foi possivel calcular sqrt(%.0lf).\n", x);
@@ -39,16 +33,12 @@ void calc_raiz(double a1, double x, int i, int e)
 
 int calc_erro(double b1, int e, double x)
 {
-  // printf("b1= %lf x= %lf\n", b1, x);
   b1 = b1 * b1;
   double erro, erromax;
   erro = x - b1;
-  // printf("erro=%.20lf\n", erro);
   if (erro < 0)
     erro = erro * (-1);
   erromax = 1.0 / pow(10, e);
-  // printf("erro=%.20lf\n", erro);
-  // printf("erromax=%.20lf\n", erromax);
   if (erro < erromax)
     return 1;
   else
@@ -75,8 +65,6 @@ int main()
   else
   {
     a = encontra_inteiros(x);
-    // printf("a = %lf\n", a);
-    // printf("a= %lf x= %lf\n", a, x);
     calc_raiz(a, (double)x, 1, e);
   }
   return 0;
