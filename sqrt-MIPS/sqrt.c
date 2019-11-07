84401219b26450c5de02932664eca7a10e099848
Mateus Oliveira <o.mateus.p@gmail.com>
2019-11-07 12:09:53 -0300

    update sqrt.c

--- sqrt-MIPS/sqrt.c
+++ sqrt-MIPS/sqrt.c
@@ -1,44 +1,32 @@
 #include <stdio.h>
+#include <math.h>
+
+void imprime_saida(double resp, int i, double x)
+{
+  printf("A raiz quadrada de %.0lf eh %.15lf, calculada em %d iteracoes.\n", x, resp, i);
+}
 
 double encontra_inteiros(double x)
 {
-  double i, i2 = 1;
-  for (i = 2; i2 < x; i++)
+  double i, somador = 1;
+  int atual = 1;
+  for (i = 0; somador < x; i++)
   {
-    i2 = i * i;
+    somador += atual;
+    atual += 2;
   }
-  i -= 2;
-  return i;
+  return i - 1;
 }
 
-void calc_raiz(double a1, double x, int i, int e)
+int calc_erro(double res, int e, double x)
 {
-  double a2, b1;
-  b1 = x / a1;
-  a2 = (a1 + b1) / 2.0;
-  if (i > 100)
-  {
-    printf("Nao foi possivel calcular sqrt(%.0lf).\n", x);
-  }
-  else if (calc_erro(b1, e, x))
-  {
-    imprime_saida(b1, i, x);
-  }
-  else
-  {
-    i++;
-    calc_raiz(a2, x, i, e);
-  }
-}
-
-int calc_erro(double b1, int e, double x)
-{
-  b1 = b1 * b1;
+  res = res * res;
   double erro, erromax;
-  erro = x - b1;
+  erro = x - res;
   if (erro < 0)
     erro = erro * (-1);
   erromax = 1.0 / pow(10, e);
+  printf("erromax = %.16lf\n", erromax);
   if (erro < erromax)
     return 1;
   else
@@ -47,9 +35,26 @@ int calc_erro(double b1, int e, double x)
   }
 }
 
-void imprime_saida(double resp, int i, double x)
+void calc_raiz(double a1, double x, int i, int e)
 {
-  printf("A raiz quadrada de %.0lf eh %.15lf, calculada em %d iteracoes.\n", x, resp, i);
+  printf("i= %d\n", i);
+  double a2, b1;
+  b1 = x / a1;
+  a2 = (a1 + b1) / 2.0;
+  printf("a= %lf b= %lf\n", a1, b1);
+  if (i > 100)
+  {
+    printf("Nao foi possivel calcular sqrt(%.0lf).\n", x);
+  }
+  else if (calc_erro(a2, e, x))
+  {
+    imprime_saida(a2, i, x);
+  }
+  else
+  {
+    i++;
+    calc_raiz(a2, x, i, e);
+  }
 }
 
 int main()
@@ -65,6 +70,7 @@ int main()
   else
   {
     a = encontra_inteiros(x);
+    printf("a= %lf\n", a);
     calc_raiz(a, (double)x, 1, e);
   }
   return 0;
